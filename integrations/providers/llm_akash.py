1ce01d3a4eee5b6744abecbb8250a694e835c870
juanarancibia <arancibia.juan22@gmail.com>
2025-05-27 20:43:57 -0300

    refactor: change llm model to a superior one

--- integrations/providers/llm_akash.py
+++ integrations/providers/llm_akash.py
@@ -1,20 +1,24 @@
-from langchain_openai import ChatOpenAI
-from integrations.llm_providers_interface import LLMClientInterface
-from config import config
 import logging
-from core.models.financial.transaction import Transaction
+
+from langchain_openai import ChatOpenAI
+
+from config import config
 from core.models.common.action_type import Actions
 from core.models.financial.forex import Forex
 from core.models.financial.investment import Investment
+from core.models.financial.transaction import Transaction
 from core.models.financial.transfer import Transfer
+from integrations.llm_providers_interface import LLMClientInterface
 
 logger = logging.getLogger(__name__)
+
 
 class AkashLLMClient(LLMClientInterface):
     """
     Client to interact with the Akash LLM API.
     Encapsulates the connection details and request logic.
     """
+
     def __init__(self):
         """
         Initializes the AkashLLMClient with the configuration from the config module.
@@ -23,12 +27,11 @@ class AkashLLMClient(LLMClientInterface):
         self.llm = ChatOpenAI(
             base_url=config.AKASH_API_BASE_URL,
             api_key=config.AKASH_API_KEY,
-            model_name="DeepSeek-R1-Distill-Qwen-14B",
-            temperature=config.LLM_TEMPERATURE
+            model_name="Qwen3-235B-A22B-FP8",
+            temperature=config.LLM_TEMPERATURE,
         )
         logger.info("Akash OpenAI client initialized successfully.")
 
-    
     def determinate_action(self, prompt: str) -> Actions:
         """
         Sends a prompt to the Akash LLM and returns the generated response.
@@ -36,8 +39,10 @@ class AkashLLMClient(LLMClientInterface):
         """
         structured_llm = self.llm.with_structured_output(Actions)
         return structured_llm.invoke(prompt)
-    
-    def generate_response(self, prompt: str, output) -> Forex | Investment | Transaction | Transfer:
+
+    def generate_response(
+        self, prompt: str, output
+    ) -> Forex | Investment | Transaction | Transfer:
         """
         Sends a prompt to the Akash LLM and returns the generated response.
         Logs the request and any errors during the API call.
